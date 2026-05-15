1cfb52d8bbd2aa456ce31ef399c629ba63be2d30
guptaprince102 <53466109+guptaprince102@users.noreply.github.com>
2026-05-16 03:48:08 +0530

    Add wait for network idle state in booking test

--- tests/Assignment1_Section9.spec.ts
+++ tests/Assignment1_Section9.spec.ts
@@ -64,6 +64,7 @@ test('Step 5 Verify Booking Confirmation in MyBookings Page', async () => {
     const myBookingPage = poManager.getMyBookingsPage();
 
     await homePage.goToMyBookings();
+    await page.waitForLoadState('networkidle');
     expect(page.url()).toBe(URL+'bookings');
     await expect(page).toHaveURL(URL+'bookings');
     
