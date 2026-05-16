cc8642903ba4453208e83184d16b474747b3475e
Prince <prince@Princes-MacBook-Air.local>
2026-05-16 23:00:36 +0530

    new branch

--- tests/Assignment1_Section9.spec.ts
+++ tests/Assignment1_Section9.spec.ts
@@ -85,7 +85,6 @@ test('Step 6 Verify Seat Count is decreased by 1 in Events Page', async () => {
     await expect(myEvent).toBeVisible();
     const seatCountAfterBooking = parseInt(await myEvent.getByText('Seat').first().innerText());
     console.log(seatCountAfterBooking);
-    //test
     
     expect(seatCountAfterBooking === seatCount-1).toBeTruthy();
 
